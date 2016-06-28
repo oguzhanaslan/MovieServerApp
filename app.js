e1b308cfb25e161ca1fd4c069c921ec536452864
Oguzhan <iletisim@oguzhanaslan.com.tr>
2016-06-28 18:16:43 +0300

    clean code part 1

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.delete('/api/movies/:id', function(req, res){
 	});
 });
 
-//  update movies
+// update movies
 app.put('/api/movies/:id', function(req, res){
 	var id = req.params._id;
 	var movie = req.body;
@@ -110,99 +110,6 @@ app.post('/api/movies', function(req, res){
 		res.json(result);
 	});
 });
-
-
-// app.delete('/api/genres/:_id', function(req, res){
-// 	var id = req.params._id;
-// 	Genre.removeGenre(id, function(err, genre){
-// 		if(err){
-// 			throw err;
-// 		}
-// 		res.json(genre);
-// 	});
-// });
-
-
-// app.get('/api/genres', function(req, res){
-// 	Genre.getGenres(function(err, genres){
-// 		if(err){
-// 			throw err;
-// 		}
-// 		res.json(genres);
-// 	});
-// });
-//
-// app.post('/api/genres', function(req, res){
-// 	var genre = req.body;
-// 	Genre.addGenre(genre, function(err, genre){
-// 		if(err){
-// 			throw err;
-// 		}
-// 		res.json(genre);
-// 	});
-// });
-//
-// app.put('/api/genres/:_id', function(req, res){
-// 	var id = req.params._id;
-// 	var genre = req.body;
-// 	Genre.updateGenre(id, genre, {}, function(err, genre){
-// 		if(err){
-// 			throw err;
-// 		}
-// 		res.json(genre);
-// 	});
-// });
-//
-// app.delete('/api/genres/:_id', function(req, res){
-// 	var id = req.params._id;
-// 	Genre.removeGenre(id, function(err, genre){
-// 		if(err){
-// 			throw err;
-// 		}
-// 		res.json(genre);
-// 	});
-// });
-//
-// app.get('/api/books', function(req, res){
-// 	Book.getBooks(function(err, books){
-// 		if(err){
-// 			throw err;
-// 		}
-// 		res.json(books);
-// 	});
-// });
-//
-// //  post
-// app.post('/api/books', function(req, res){
-// 	var book = req.body;
-// 	Book.addBook(book, function(err, book){
-// 		if(err){
-// 			throw err;
-// 		}
-// 		res.json(book);
-// 	});
-// });
-//
-// app.put('/api/books/:_id', function(req, res){
-// 	var id = req.params._id;
-// 	var book = req.body;
-// 	Book.updateBook(id, book, {}, function(err, book){
-// 		if(err){
-// 			throw err;
-// 		}
-// 		res.json(book);
-// 	});
-// });
-//
-// app.delete('/api/books/:_id', function(req, res){
-// 	var id = req.params._id;
-// 	Book.removeBook(id, function(err, book){
-// 		if(err){
-// 			throw err;
-// 		}
-// 		res.json(book);
-// 	});
-// });
 
 app.listen(3000);
 console.log('Running on port 3000...');
